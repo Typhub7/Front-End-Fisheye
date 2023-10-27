cc31c3bc24ce6d603dbc5dab02e39680a82ec936
Typho7 <caro.d@free.fr>
2023-10-27 21:27:19 +0200

    Gh page adaptation for api

--- scripts/api/photographerData.js
+++ scripts/api/photographerData.js
@@ -1,15 +1,16 @@
 export async function fetchPhotographersData() {
   try {
-    const response = await fetch('../data/photographers.json'); // https://raw.githubusercontent.com/{username}/{repo}/{branch}/{file}
+    const response = await fetch('../data/photographers.json'); 
 
     if (!response.ok) {
-      const response = await import('../data/photographers.json'); // Only for gh-pages - Remove when back-end will be ok
       throw new Error('Erreur lors de la récupération des données des photographes.');
     }
+
     const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
-    return [];
+    const fallbackResponse = await import('../data/photographers.json'); // Only for Gh Page Working -- Remove When back end is ready
+    return fallbackResponse; // Only for Gh Page Working -- only put return[] When back end is ready
   }
 }
