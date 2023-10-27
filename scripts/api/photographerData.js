0e3a2ec76aee7321657819f8b6b96f7e4c39da14
Typho7 <caro.d@free.fr>
2023-10-27 21:14:30 +0200

    fetch function adaptation for ghpage

--- scripts/api/photographerData.js
+++ scripts/api/photographerData.js
@@ -1,8 +1,9 @@
 export async function fetchPhotographersData() {
   try {
-    const response = await fetch('../data/photographers.json');
+    const response = await fetch('../data/photographers.json'); // https://raw.githubusercontent.com/{username}/{repo}/{branch}/{file}
 
     if (!response.ok) {
+      const response = await import('../data/photographers.json'); // Only for gh-pages - Remove when back-end will be ok
       throw new Error('Erreur lors de la récupération des données des photographes.');
     }
     const data = await response.json();
