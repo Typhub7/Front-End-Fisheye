b3def2e889fd86fb29a736f46f020f6688855a19
Typho7 <caro.d@free.fr>
2023-10-30 09:18:42 +0100

    gh-page

--- scripts/api/photographerData.js
+++ scripts/api/photographerData.js
@@ -1,6 +1,6 @@
 export async function fetchPhotographersData() {
   try {
-    const response = await fetch('../data/photographers.json'); 
+    const response = await fetch('/data/photographers.json'); 
 
     if (!response.ok) {
       throw new Error('Erreur lors de la récupération des données des photographes.');
@@ -10,7 +10,7 @@ export async function fetchPhotographersData() {
     return data;
   } catch (error) {
     console.error(error);
-    const fallbackResponse = await fetch('https://typhub7.github.io/Front-End-Fisheye/data/photographers.json'); // Only for Gh Page Working -- Remove When back end is ready
-    return fallbackResponse; // Only for Gh Page Working -- only put return[] When back end is ready
+    //const fallbackResponse = await fetch('https://typhub7.github.io/Front-End-Fisheye/data/photographers.json'); // Only for Gh Page Working -- Remove When back end is ready
+    //return fallbackResponse; // Only for Gh Page Working -- only put return[] When back end is ready
   }
 }
