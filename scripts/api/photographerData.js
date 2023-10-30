31d5211df3a7ab953a3574f2f96b038d5e5304b3
Typho7 <caro.d@free.fr>
2023-10-30 10:22:47 +0100

    last gh page page adjustment

--- scripts/api/photographerData.js
+++ scripts/api/photographerData.js
@@ -10,7 +10,5 @@ export async function fetchPhotographersData() {
     return data;
   } catch (error) {
     console.error(error);
-    //const fallbackResponse = await fetch('https://typhub7.github.io/Front-End-Fisheye/data/photographers.json'); // Only for Gh Page Working -- Remove When back end is ready
-    //return fallbackResponse; // Only for Gh Page Working -- only put return[] When back end is ready
   }
 }
